e9192bcede0917a2d4562a167780dbe6e48a829a
Bikalpa KC <bkalpa1921@gmail.com>
2024-07-01 17:32:03 +0545

    back to top

--- script.js
+++ script.js
@@ -153,3 +153,14 @@ jQuery(document).ready(function($) {
       }
   });
 });
+
+// scroll to top
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 500 ) {
+      $('.scrolltop:hidden').stop(true, true).fadeIn();
+  } else {
+      $('.scrolltop').stop(true, true).fadeOut();
+  }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".image-slider").offset().top},"1000");return false})})
